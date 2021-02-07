002dafe63ed973dbb16c83decc564b1394eff8ed
Gagou <gaelle.shearn.jmr@gmail.com>
2021-02-07 19:04:21 +0100

    controller audiobooks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :audiobooks, only: [:index, :new, :create, :edit, :update, :destroy]
+  # -> ça veut dire toutes mdrr
 end
